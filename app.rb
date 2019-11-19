5dcd826b83f198b17cad0e898c86d64942bb0cb2
Steven Laff <steve.laff@gmail.com>
2019-11-19 04:26:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_phrase = params[:user_phrase.downcase]
+    word = PigLatinizer.new
+    @latinized_word = word.piglatinize(user_phrase)
+    erb :piglatinize
+  end
 end
